a2c9c3939ec9cf68ab5e7fb144b280d7b40b92a8
Bhargavi <Bhargavi@DESKTOP-FBVFHMK>
2026-01-22 22:22:40 +0530

    Day 9 Tns programs

---
+++ TNS/src/programs/Day9/ThrowsDemo.java
@@ -0,0 +1,5 @@
+package programs.Day9;
+
+public class ThrowsDemo {
+
+}
---
+++ TNS/src/programs/Day9/ThrowsDemomain.java
@@ -0,0 +1,10 @@
+package programs.Day9;
+
+public class ThrowsDemomain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

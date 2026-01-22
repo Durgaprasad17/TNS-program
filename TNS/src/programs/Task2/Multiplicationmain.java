32e6f180e820c1f7490cca31d90f51b0665d1a0d
Bhargavi <Bhargavi@DESKTOP-FBVFHMK>
2026-01-22 23:20:19 +0530

    Task2 Programs

--- TNS/src/programs/Task2/Multiplicationmain.java
+++ TNS/src/programs/Task2/Multiplicationmain.java
@@ -1,5 +1,15 @@
 package programs.Task2;
+import java.util.Scanner;
 
 public class Multiplicationmain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
+        Multiplicationtabl mt = new Multiplicationtabl(); 
+        mt.table(number);
+
+        sc.close(); // good practice to close Scanner
+    }
 }
--- TNS/src/programs/Task2/Multiplicationtabl.java
+++ TNS/src/programs/Task2/Multiplicationtabl.java
@@ -1,5 +1,9 @@
 package programs.Task2;
 
-public class Multiplicationtabl {
-
+public class Multiplicationtabl {   // fixed name
+    void table(int num) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
 }

9886e77edf6df940341567ffdaa6fe7d3287307e
Bhargavi <Bhargavi@DESKTOP-FBVFHMK>
2026-01-22 13:19:27 +0530

    Day 2 Tns programs

---
+++ TNS/src/programs/Day2/EEE.java
@@ -0,0 +1,23 @@
+package programs.Day2;
+
+public class EEE {
+	String name;
+    int id;
+
+    void display() {
+        System.out.println("EEE Name: " + name);
+        System.out.println("EEE ID: " + id);
+    }
+
+    void EC() {
+        System.out.println("EEE - EC Subject");
+    }
+
+    void PowerSystems() {
+        System.out.println("EEE - power systems Subject");
+    }
+
+    void ControlSystems() {
+        System.out.println("EEE - control systems Subject");
+    }
+}
---
+++ TNS/src/programs/Day2/sriindu.java
@@ -0,0 +1,38 @@
+package programs.Day2;
+
+public class sriindu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 CSE cse = new CSE();
+	        cse.name = "Chandu";
+	        cse.id = 101;
+     cse.display();
+     cse.DBMS();
+     cse.Java();
+     cse.Python();
+
+     System.out.println();
+     EEE eee = new EEE();
+     eee.name = "Navya";
+     eee.id = 105;
+
+     eee.display();
+     eee.EC();
+     eee.PowerSystems();
+     eee.ControlSystems();
+
+     System.out.println();
+     ECE ece = new ECE();
+     ece.name = "Shiva";
+     ece.id = 101;
+
+     ece.display();
+     ece.VLSI();
+     ece.ES();
+     ece.DSP();
+		
+
+	}
+	}
+
